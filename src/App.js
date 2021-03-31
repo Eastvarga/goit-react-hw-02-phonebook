b6abac1b73dfbd8fe95a0b6efb2836a4c1f194f0
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-03-31 22:40:03 +0300

    stash not usable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import Form from "./components/Form";
+import ContactList from "./components/ContactList";
+import { v4 as uuidv4 } from "uuid";
+class App extends Component {
+  state = {
+    contacts: [],
+    name: "",
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  formSubmitHandler = ({ name }) => {
+    this.setState((prevState) => {
+      return {
+        contacts: [{ id: uuidv4(), name: name }, ...prevState.contacts],
+      };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Form onSubmit={this.formSubmitHandler} />
+        <ContactList contacts={this.state.contacts} />
+      </div>
+    );
+  }
 }
 
 export default App;
